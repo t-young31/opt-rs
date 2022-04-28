033b565fd7b0a2aff174ae946a94ff93f6c2e23e
tom <tom.young@chem.ox.ac.uk>
2022-04-28 16:19:03 +0100

    Start dihedral derivative

--- src/ff/derivatives.py
+++ src/ff/derivatives.py
@@ -12,6 +12,10 @@ x_k = sym.Symbol('x_k')
 y_k = sym.Symbol('y_k')
 z_k = sym.Symbol('z_k')
 
+x_l = sym.Symbol('x_l')
+y_l = sym.Symbol('y_l')
+z_l = sym.Symbol('z_l')
+
 
 def theta():
     """Angle between two vectors j-->i  and j-->k"""
@@ -51,6 +55,81 @@ def angle_type_b(symbol):
 
     theta_ = theta()
     res = sym.diff(k*(c0 + c1*sym.cos(theta_) + c2*sym.cos(2*theta_)), symbol)
+    string = rust_expression(function=res)
+    # string = rust_expression(function=sym.refine(res, sym.Q.real(symbol)))
+    string = string.replace('__', '')
+
+    return f'gradient[self.{str(symbol)[-1]}].{str(symbol)[0]} += {string}'
+
+
+def dihedral(symbol):
+
+    r_ij_x = x_i - x_j
+    r_ij_y = y_i - y_j
+    r_ij_z = z_i - z_j
+
+    r_lk_x = x_l - x_k
+    r_lk_y = y_l - y_k
+    r_lk_z = z_l - z_k
+
+    r_kj_x = x_k - x_j
+    r_kj_y = y_k - y_j
+    r_kj_z = z_k - z_j
+
+    def cross_x(u_x, u_y, u_z, v_x, v_y, v_z):
+        return u_y*v_z - u_z*v_y
+
+    def cross_y(u_x, u_y, u_z, v_x, v_y, v_z):
+        return u_z*v_x - u_x*v_z
+
+    def cross_z(u_x, u_y, u_z, v_x, v_y, v_z):
+        return u_x*v_y - u_y*v_x
+
+    def mod(u_x, u_y, u_z):
+        return sym.sqrt(u_x**2 + u_y**2 + u_z**2)
+
+    def dot(u_x, u_y, u_z, v_x, v_y, v_z):
+        return u_x*v_x + u_y*v_y + u_z*v_z
+
+    v0_x = cross_x(r_ij_x, r_ij_y, r_ij_z, r_kj_x, r_kj_y, r_kj_z)
+    v0_y = cross_y(r_ij_x, r_ij_y, r_ij_z, r_kj_x, r_kj_y, r_kj_z)
+    v0_z = cross_z(r_ij_x, r_ij_y, r_ij_z, r_kj_x, r_kj_y, r_kj_z)
+    mod_v0 = mod(v0_x, v0_y, v0_z)
+
+    v0_x /= mod_v0
+    v0_y /= mod_v0
+    v0_z /= mod_v0
+
+    v1_x = cross_x(-r_kj_x, -r_kj_y, -r_kj_z, r_lk_x, r_lk_y, r_lk_z)
+    v1_y = cross_y(-r_kj_x, -r_kj_y, -r_kj_z, r_lk_x, r_lk_y, r_lk_z)
+    v1_z = cross_z(-r_kj_x, -r_kj_y, -r_kj_z, r_lk_x, r_lk_y, r_lk_z)
+    mod_v1 = mod(v1_x, v1_y, v1_z)
+    v1_x /= mod_v1
+    v1_y /= mod_v1
+    v1_z /= mod_v1
+
+    mod_r_kj = mod(r_kj_x, r_kj_y, r_kj_z)
+    r_kj_x /= mod_r_kj
+    r_kj_y /= mod_r_kj
+    r_kj_z /= mod_r_kj
+
+    """
+    value = -np.arctan2(np.dot(np.cross(vec1, vec_xy), vec2),
+                    np.dot(vec1, vec2))
+    """
+    v2_x = cross_x(v0_x, v0_y, v0_z, r_kj_x, r_kj_y, r_kj_z)
+    v2_y = cross_y(v0_x, v0_y, v0_z, r_kj_x, r_kj_y, r_kj_z)
+    v2_z = cross_z(v0_x, v0_y, v0_z, r_kj_x, r_kj_y, r_kj_z)
+
+    phi = -sym.atan2(dot(v2_x, v2_y, v2_z, v1_x, v1_y, v1_z),
+                     dot(v0_x, v0_y, v0_z, v1_x, v1_y, v1_z))
+
+    n = sym.Symbol("self.n_phi")
+    phi0 = sym.Symbol("self.phi0")
+
+    res = (0.5 * sym.Symbol("self.v_phi")
+           * (1 - sym.cos(n*phi0)*sym.cos(n*phi)))
+
     string = rust_expression(function=res)
     # string = rust_expression(function=sym.refine(res, sym.Q.real(symbol)))
     string = string.replace('__', '')
@@ -98,7 +177,7 @@ def rust_expression(function):
     string = string.replace('/2', '/2.')
     string = string.replace('2*', '2.*')
 
-    for fn in ('sqrt', 'acos', 'cos', 'asin', 'sin'):
+    for fn in ('sqrt', 'atan2',  'acos', 'cos', 'asin', 'sin'):
         string = convert_to_trait(string, fn)
 
     return string
@@ -145,7 +224,7 @@ def extract_variables(_lines):
 
     first_line = non_var_lines[0]
     start_idx = 0 if '=' not in first_line else len(first_line.split('= ')[0])
-    end_idx = min([len(l) for l in non_var_lines])
+    end_idx = min([len(l) for l in non_var_lines])//2
 
     for i in range(start_idx, end_idx):
 
@@ -176,7 +255,7 @@ def extract_variables(_lines):
         _lines[n_var_lines+i] = line.replace(smallest_var, var_name)
 
     if '.' not in smallest_var:
-        # Remove unnesacerray parentheses
+        # Remove unnecessary parentheses
         smallest_var = smallest_var[1:-1]
 
     _lines.insert(n_var_lines, f'let {var_name} = {smallest_var}')
@@ -197,7 +276,21 @@ def print_angle_gradient(angle_type):
     return print(";\n".join(lines))
 
 
+def print_dihedral_gradient():
+
+    lines = []
+
+    for x in (x_i, y_i, z_i, x_j, y_j, z_j, x_k, y_k, z_k, x_l, y_l, z_l):
+        lines.append(dihedral(x))
+
+    for _ in range(20):
+        extract_variables(lines)
+
+    return print(";\n".join(lines))
+
+
 if __name__ == '__main__':
 
-    print_angle_gradient(angle_type=angle_type_a)
+    # print_angle_gradient(angle_type=angle_type_a)
     # print_angle_gradient(angle_type=angle_type_b)
+    print_dihedral_gradient()
