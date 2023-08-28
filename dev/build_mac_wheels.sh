fa6de62e6e95fa4c75412cd83f0f7e935ac20433
tom <tom.young.316@gmail.com>
2023-08-28 20:45:25 +0100

    Add build workflow

--- dev/build_mac_wheels.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-for minor_version in {8..10}; do
-    conda create -n test "python=3.$minor_version" --yes
-    conda activate test
-    pip install maturin
-    maturin build --release --strip --universal2
-    conda deactivate
-done
